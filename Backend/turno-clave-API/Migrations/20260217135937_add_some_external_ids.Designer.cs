20fb5bc057ecb07d8a9f5fbbaa3071519eb42959
Santiago Brasca <santibrasca02@gmail.com>
2026-02-17 11:19:06 -0300

    add connection string to User Secrets and create external ids migration

--- Backend/turno-clave-API/Migrations/20260217135937_add_some_external_ids.Designer.cs
+++ Backend/turno-clave-API/Migrations/20260217135937_add_some_external_ids.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using turno_clave_API.Infrastructure.Data;
@@ -11,9 +12,11 @@ using turno_clave_API.Infrastructure.Data;
 namespace turno_clave_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217135937_add_some_external_ids")]
+    partial class add_some_external_ids
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
