e497441fa0d8c6876e2dfe1040631bb4113852a0
Santiago Brasca <santibrasca02@gmail.com>
2026-04-14 18:43:10 -0300

    disable https redirection

--- Backend/turno-clave-API/Program.cs
+++ Backend/turno-clave-API/Program.cs
@@ -140,10 +140,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-if (!app.Environment.IsDevelopment())
-{
-app.UseHttpsRedirection();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//app.UseHttpsRedirection();
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
